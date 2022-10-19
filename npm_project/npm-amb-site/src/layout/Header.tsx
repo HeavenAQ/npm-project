d913ae1344c22315d01f6e8a62a4990e1c446679
heavenchen <891118heaven@gmail.com>
2022-10-20 01:57:33 +0800

    Add time and location to header

--- npm_project/npm-amb-site/src/layout/Header.tsx
+++ npm_project/npm-amb-site/src/layout/Header.tsx
@@ -8,6 +8,12 @@ const Header: React.FC = (): JSX.Element => {
 				<span className="primary__heading--mid">南巡</span>
 				<span className="primary__heading--sub">一場玩味文物的旅行</span>
 			</h1>
+			<div className="primary__heading--location">國立中山大學</div>
+			<h3 className="tertiary__heading">
+				<div>共同主辦 - 國立中山大學圖書與資訊處</div>
+				<div>11/1 - 11/17 圖資大樓一樓入口前廊</div>
+				<div>11/15  14:00 工作坊圖資大樓二樓研討室</div>
+			</h3>
 		</header>
 	)
 }
