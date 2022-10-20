d0367716b6a8b91a49c44613c518cfd8a3bef1d6
heavenchen <891118heaven@gmail.com>
2022-10-20 14:56:47 +0800

    Add carousel pics

---
+++ npm_project/npm-amb-site/src/sass/scss/images/intro/IntroMedia.tsx
@@ -0,0 +1,9 @@
+import media1 from './犧尊.png';
+import media2 from './頸枕.png';
+import media3 from './文物架.png';
+import media4 from './漁夫帽.png';
+import media5 from './傘.png';
+
+
+export const media = [media1, media2, media3, media4, media5];
+export const introMediaByIndex = (i:number) => media[i];
